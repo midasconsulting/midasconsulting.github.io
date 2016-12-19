a340e8eda350283a908ef3a5a5749dcf51713beb
David <hyhdvd@gmail.com>
2016-12-18 17:42:14 -0800

    polish forms

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,12 @@
+sr.reveal('.js-reveal',
+{
+  distance: '7.5rem',
+  scale: 1,
+  viewFactor: .1,
+  duration: 750,
+  opacity: 0
+});
+
 $(document).ready(function(){
   $(".js-btn").click(function(){
     $(".js-btn-main").slideToggle();
